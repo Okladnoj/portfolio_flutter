17cc042a14f49d71048a2c660f54b9e42e606a80
Дмитрий <36802187+Okladnoj@users.noreply.github.com>
2020-01-29 12:27:05 +0200

    Update main.dart

--- test_application/lib/main.dart
+++ test_application/lib/main.dart
@@ -13,37 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ClasTest(),
+      home: MyHomePage(),
     );
   }
 }
-
-class ClasTest extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return Text('String: $index');
-        },
-      ),
-    );
-  }
-}
-
-List<Widget> _myList = [
-  Text('line1'),
-  Divider(),
-  Text('line1'),
-  Divider(),
-  Text('line1'),
-  Divider(),
-  Text('line1'),
-  Divider(),
-  Text('line1'),
-  Divider(),
-  Text('line1'),
-  Divider(),
-  Text('line1'),
-  Divider(),
-];
